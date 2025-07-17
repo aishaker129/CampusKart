bd3f502b976070a6ff607a2e32eef5214267be18
Shaker <aishaker129@gmail.com>
2025-07-17 19:44:15 +0600

    initialize campusKart server

---
+++ CampusKart_Server/src/test/java/com/campusKart/CampusKartServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusKartServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
