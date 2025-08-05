78d085ae1252353cd4d411c5d2762f6df991d824
aishaker129 <aishaker129@gmail.com>
2025-08-05 15:09:17 +0600

    Add Railway deployment config and ignore .env

--- CampusKart_Server/src/test/java/com/campusKart/CampusKartServerApplicationTests.java
+++ CampusKart_Server/src/test/java/com/campusKart/CampusKartServerApplicationTests.java
@@ -1,13 +1,28 @@
+// File: src/test/java/com/campusKart/CampusKartServerApplicationTests.java
+
 package com.campusKart;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CampusKartServerApplicationTests {
+public class CampusKartServerApplicationTests {
+
+	@BeforeAll
+	static void loadEnv() {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
+		System.setProperty("CLOUDINARY_KEY", dotenv.get("CLOUDINARY_KEY"));
+		System.setProperty("CLOUDINARY_SECRET", dotenv.get("CLOUDINARY_SECRET"));
+	}
 
 	@Test
 	void contextLoads() {
+		// test whether context loads
 	}
-
 }
